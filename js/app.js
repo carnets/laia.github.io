107c17ff5337746143f784e6ce126da0aa06b016
Marc Garcia <mgm793@gmail.com>
2017-11-18 14:18:38 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 var APP = {
 	init: function(){
-
+		if(navigator.userAgent.match(/Android/i)){
+    		window.scrollTo(0,1);
+ 		}
+		$(document).on("click", ".container", function () {
+    		$(this).toggleClass('hover');
+		});
 	}
 }
 APP.init();
